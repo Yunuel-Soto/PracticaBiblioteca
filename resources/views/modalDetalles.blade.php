03935c4c0f7e664e9fb71e98fbdeb3594d28a6ba
Yunuel Soto <brianyunuelsotosanchezz@gmail.com>
2022-12-01 17:49:53 -0600

    Ultimo cambio estetico minimo xd

--- resources/views/modalDetalles.blade.php
+++ resources/views/modalDetalles.blade.php
@@ -7,13 +7,13 @@
                     <h3 class="modal-title" id="staticBackdropLabel">Detalles</h3>
                 </div>
                 <div class="modal-body">
-                    <h3 class="eliminlabel">{{$consulta->titulo}}</h3>
-                    <h5 class="eliminlabel">Autor: {{$consulta->autor}}</h5>
-                    <h5 class="eliminlabel">ISBN: {{$consulta->isbn}}</h5>
-                    <h5 class="eliminlabel">Editorial: {{$consulta->editorial}}</h5>
-                    <h5 class="eliminlabel">No Paginas: {{$consulta->paginas}}</h5>
-                    <h5 class="eliminlabel">Contacto: {{$consulta->email}}</h5>
-                    <h5 class="eliminlabel">Fecha del registro: {{$consulta->fecha}}</h5>
+                    <h3 class="eliminlabel e1">{{$consulta->titulo}}</h3>
+                    <h5 class="eliminlabel e2">Autor: {{$consulta->autor}}</h5>
+                    <h5 class="eliminlabel e3">ISBN: {{$consulta->isbn}}</h5>
+                    <h5 class="eliminlabel e4">Editorial: {{$consulta->editorial}}</h5>
+                    <h5 class="eliminlabel e5">No Paginas: {{$consulta->paginas}}</h5>
+                    <h5 class="eliminlabel e6">Contacto: {{$consulta->email}}</h5>
+                    <h5 class="eliminlabel e7">Fecha del registro: {{$consulta->fecha}}</h5>
 
                 </div>
                 <div class="modal-footer">
